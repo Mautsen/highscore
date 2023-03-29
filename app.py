483d9364f5139293e05458ef1f4f215c2e35b5e0
Matias Leppänen <matias.joosua@hotmail.com>
2023-03-29 13:49:51 +0300

    Done

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import json
 app = Flask(__name__)
 
 the_id=2
-customers = [{"id": 1, "name": "jack"}, {"id": 2, "name": "hannah"}]
+scores = [{"id": 1, "name": "jack"}, {"id": 2, "name": "hannah"}]
 
 @app.after_request
 def after_request(response):
@@ -13,34 +13,37 @@ def after_request(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
-@app.route("/customers")
+@app.route("/scores")
 def get_customers():
     read_database()
-    return jsonify(customers)
+    return jsonify(scores
+)
 
-@app.route('/customers/<int:the_id>')
+@app.route('/scores/<int:the_id>')
 def get_customer(the_id):
     read_database()
     customer = -1
-    for i in range (0,len(customers)):
-        if(customers[i]["id"] == the_id):
+    for i in range (0,len(scores)):
+        if(scores[i]["id"] == the_id):
             customer = i
             break
     if customer != -1:
-        for i in range(customer, len(customers)):
-            customers[i]['id'] = customers[i]['id'] - 1
-        return jsonify(customers[customer])
+        for i in range(customer, len(scores)):
+            scores[i]['id'] = scores[i]['id'] - 1
+        return jsonify(scores
+    [customer])
     else:
         return make_response(jsonify(message="Customer not found"), 404)
 
-@app.route('/customers/<int:the_id>', methods = ['DELETE'])
+@app.route('/scores/<int:the_id>', methods = ['DELETE'])
 def delete_customer(the_id):
     # initialize customer index
     customer = -1
     
-    # loop through customers list to find matching id
-    for i in range (0,len(customers)):
-        if(customers[i]["id"] == the_id):
+    # loop through scores
+    # list to find matching id
+    for i in range (0,len(scores)):
+        if(scores[i]["id"] == the_id):
             # if found, set customer index to current index i and exit loop
             customer = i
             break
@@ -48,11 +51,12 @@ def delete_customer(the_id):
     # if customer with given id exists
     if customer != -1:
         # remove the customer from the list
-        customers.pop(customer)
+        scores.pop(customer)
         
-        # update the id of remaining customers
-        for i in range(customer, len(customers)):
-            customers[i]['id'] = customers[i]['id'] - 1
+        # update the id of remaining scores
+    
+        for i in range(customer, len(scores)):
+            scores[i]['id'] = scores[i]['id'] - 1
         
         # return 204 status code (No Content)
         return make_response("", 204)
@@ -60,16 +64,19 @@ def delete_customer(the_id):
         # if customer not found, return 404 status code (Not Found) with error message
         return make_response(jsonify(message="Customer not found"), 404)
 
-@app.route('/customers', methods=['POST'])
+@app.route('/scores', methods=['POST'])
 def add_customer():
     global the_id # Global variable to keep track of the ID of the new customer
-    global customers # Global list to store the existing customers
+    global scores
+ # Global list to store the existing scores
+
     
     customer = json.loads(request.data) # Get the customer data from the request and convert it to a Python dictionary using the json module
     
     the_id += 1 # Increment the ID to generate a new ID for the new customer
     customer['id'] = the_id # Assign the new ID to the customer
-    customers.append(customer) # Add the new customer to the list of customers
+    scores.append(customer) # Add the new customer to the list of scores
+
     save_to_database(customer) # Save the new customer to the database
     
     return make_response("", 201) # Return a HTTP 201 Created status code to indicate that the customer was successfully added
