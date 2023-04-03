dc4a548a4675d950541564ec667e69490bf5aff5
JonnaKyl <jonna.kyllonen1@gmail.com>
2023-04-03 11:05:03 +0300

    Done

--- app.py
+++ app.py
@@ -15,11 +15,17 @@ def after_request(response):
 
 # JONNA read_scores
 def read_scores():
-    # avataan json-tiedosto ja puretaan sen sisältö listaksi
     try:
         with open('scores.txt', 'r') as f:
-            scores = json.load(f)
-    # jos tiedostoa ei löydy, palautetaan tyhjä lista
+            file_contents = f.read()
+            if file_contents.strip() == '':
+                # tiedosto on tyhjä, palautetaan tyhjä lista
+                return []
+            else:
+                scores = json.loads(file_contents)
+                if isinstance(scores, dict):
+                    # jos json-tiedosto sisältää yhden nimen, muutetaan se listaksi
+                    scores = [scores]
     except FileNotFoundError:
         scores = []
     return scores
