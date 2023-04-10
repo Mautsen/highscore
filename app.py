8fdbe28123f05e413e69e18f9c31306e4720c884
JonnaKyl <jonna.kyllonen1@gmail.com>
2023-04-10 11:48:15 +0300

    Done

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, Response, jsonify, request, make_response, render_template
 import json
 from operator import itemgetter
-from repository import read_scores, save_scores
+from repository import *
 
 
 app = Flask(__name__)
@@ -39,7 +39,8 @@ def get_scores():
 # JONNA "Fetching score based on id": 
 @app.route('/scores/<int:the_id>')
 def get_scores_id(the_id):
-    scores = read_scores()
+    # scores = read_scores()
+    scores = fetch_scores()
     for score in scores:
         if score["id"] == the_id:
             return jsonify(score), 200
@@ -50,7 +51,8 @@ def get_scores_id(the_id):
 def add_score():
     # load given string and turn it into dictionary
     score = json.loads(request.data)
-    scores = read_scores()
+    # scores = read_scores()
+    scores = fetch_scores()
     # generate new score ID
     if scores:
         score_id = scores[-1]['id'] + 1
@@ -60,7 +62,7 @@ def add_score():
     score['id'] = score_id
     scores.append(score)
     # save updated scores
-    save_scores(scores)
+    save_to_scores(scores)
     # return success response
     return make_response("", 201)
 
@@ -77,7 +79,7 @@ def delete_customer(the_id):
     if(index_to_be_deleted != -1):
         scores.pop(index_to_be_deleted)
         # tallennetaan
-        save_scores(scores)
+        save_to_scores(scores)
         return make_response("", 204)
     else:
         return make_response("", 404)
@@ -131,34 +133,34 @@ def limit():
 
     return jsonify(results), 200
 
-# @app.route('/', methods = ['POST', 'GET'])
-# def index():
-#     """
-#     The index function that handles both GET and POST requests to the root route. When a GET request is made,
-#     the function reads the data from the database and renders the index.html template with an empty form.
-#     When a POST request is made, the function reads the form data, validates the name and saves the data to the database.
-#     It then reads the updated data from the database and renders the index.html template with the updated data.
+@app.route('/', methods = ['POST', 'GET'])
+def index():
+    """
+    The index function that handles both GET and POST requests to the root route. When a GET request is made,
+    the function reads the data from the database and renders the index.html template with an empty form.
+    When a POST request is made, the function reads the form data, validates the name and saves the data to the database.
+    It then reads the updated data from the database and renders the index.html template with the updated data.
 
-#     Returns:
-#         str: the rendered HTML template as a string.
-#     """ 
-#     if request.method == 'POST':
-#         id = request.form.get('id') # get user input from post
-#         name = request.form.get('name')
-#         points=request.form.get('points')
-#         name = f"{name}" 
-#         save_scores(id, name, points)
-#         read_scores()   
-#         return render_template('scores.html', name=str(name), id=id, points=points)
-#         #else:
-#             #raise Exception("Give a proper name for example 'John Wick'")
-#     else:
-#         scores = read_scores()
-#         for score in scores:
-#             score['id'] = score.pop('id')
-#             score['name'] = score.pop('name')
-#             score['points'] = score.pop('points')
-#         return render_template('scores.html', scores=scores)
+    Returns:
+        str: the rendered HTML template as a string.
+    """ 
+    if request.method == 'POST':
+        id = request.form.get('id') # get user input from post
+        name = request.form.get('name')
+        points=request.form.get('points')
+        name = f"{name}" 
+        save_scores(id, name, points)
+        read_scores()   
+        return render_template('scores.html', name=str(name), id=id, points=points)
+        #else:
+            #raise Exception("Give a proper name for example 'John Wick'")
+    else:
+        scores = read_scores()
+        for score in scores:
+            score['id'] = score.pop('id')
+            score['name'] = score.pop('name')
+            score['points'] = score.pop('points')
+        return render_template('scores.html', scores=scores)
 
 if __name__ == "__main__":
     app.run()
--- repository.py
+++ repository.py
@@ -1,26 +1,74 @@
 import json
 
-# JONNA read_scores
 def read_scores():
-    try:
-        with open('scores.txt', 'r') as f:
-            file_contents = f.read()
-            if file_contents.strip() == '':
-                # tiedosto on tyhjä, palautetaan tyhjä lista
-                return []
-            else:
-                scores = json.loads(file_contents)
-                if isinstance(scores, dict):
-                    # jos json-tiedosto sisältää yhden nimen, muutetaan se listaksi
-                    scores = [scores]
-    except FileNotFoundError:
-        scores = []
-    return scores
+    data = open("database.txt", "r")
+    return data.read()
 
-# JONNA save_scores to the scores.txt
-def save_scores(scores):
-    # tallennetaan tiedot json-muodossa tiedostoon
-    with open('scores.txt', 'w') as f:
-        json.dump(scores, f)
-    # with open("database.txt", 'a') as f:
-    #     f.write(f"\n{id},{name},{points}")
+def save_to_scores(name, id):
+    f = open("database.txt", "a")
+    f.write(f"\n{id},{name}")
+    f.close()
+
+#used with delete
+def overwrite_scores(db_list):
+    f = open("database.txt", "w")
+    for i in range(len(db_list)):
+        id = db_list[i]['id']
+        name = db_list[i]['name']
+        if len(db_list) - 1 > i:
+            f.writelines(str(id) + ',' + name + '\n')
+        else:
+            #last line of database.txt must not be empty,
+            #pr it will break the code
+            f.writelines(str(id) + ',' + name)
+    f.close()
+
+def fetch_scores():
+    customers = []
+    database_content = read_scores()
+    db_split = database_content.split('\n')
+    for data in db_split:
+        attributes = data.split(',')
+        customer = {'id': int(attributes[0]), 'name': attributes[1]}
+        customers.append(customer)
+    return customers
+
+def main():
+    print(read_scores())
+
+if __name__ == "__main__":
+    main()
+
+# # JONNA read_scores
+# def read_scores():
+#     try:
+#         with open('scores.txt', 'r') as f:
+#             file_contents = f.read()
+#             if file_contents.strip() == '':
+#                 # tiedosto on tyhjä, palautetaan tyhjä lista
+#                 return []
+#             else:
+#                 scores = json.loads(file_contents)
+#                 if isinstance(scores, dict):
+#                     # jos json-tiedosto sisältää yhden nimen, muutetaan se listaksi
+#                     scores = [scores]
+#     except FileNotFoundError:
+#         scores = []
+#     return scores
+
+# # JONNA save_scores to the scores.txt
+# def save_scores(scores):
+#     # tallennetaan tiedot json-muodossa tiedostoon
+#     with open('scores.txt', 'w') as f:
+#         json.dump(scores, f)
+#     # with open("database.txt", 'a') as f:
+#     #     f.write(f"\n{id},{name},{points}")
+
+# def fetch_customers():
+#     scores = []
+#     with open('scores.txt', 'r') as f:
+#         scores = json.load(f)
+#     for data in scores:
+#         customer = {'id': int(data['id']), 'name': data['name'], 'points': data['points']}
+#         scores.append(customer)
+#     return scores
