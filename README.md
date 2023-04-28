c7c1a79c355d0b0dfbedaca54e22db0623208ec4
JonnaKyl <jonna.kyllonen1@gmail.com>
2023-04-28 09:25:20 +0300

    Added client and info in readme.md

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # highscore
 Highscore for mobilegame
 
-Author
-Jonna Kyllönen
+# Author
+Jonna Kyllönen, 
 Matias Leppänen
 
-(Screenshots)
+# Screenshots
+![Image of highscore](/highscore.png)
 
 
-Tech/framework used
+# Tech/framework used
 
 Python
 HTML
@@ -18,24 +19,20 @@ CSS
 External modules:
 
 - `flask`: a web framework for Python.
-- `json`: a built-in Python module for working with JSON data.
-- `operator`: a built-in Python module that provides functions for performing operations on data types such as lists, dictionaries, and tuples.
 - `requests`: a third-party Python library for sending HTTP requests.
 - `flask_bcrypt`: a third-party Python library for encrypting passwords using bcrypt hashing.
 - `firebase_admin`: a third-party Python library for integrating with the Firebase platform.
-- `re`: a built-in Python module for working with regular expressions.
-- `typing`: a built-in Python module for adding type hints to function parameters and return values.
 - `Gunicorn`: Python HTTP server
 
-Installation and running
-Provide step by step series of examples and explanations about how to get a development env running. Example:
+# Installation and running:
+See the requirements.txt file or above which external modules are necessary to install before running the code and install them. Command: pip install <module name here>
 
-# clone repo
-git clone highscore (More instructions from here: https://docs.github.com/en/repositories/creating-and-managing-repositories/cloning-a-repository)
+# clone highscore repository
+git clone <copy the repository link here> (More instructions from here: https://docs.github.com/en/repositories/creating-and-managing-repositories/cloning-a-repository)
 cd highscore
 
 # start backend
-python client.py
+The file app.py cannot be run at the moment because of firebase. The file client.py uses the backend functions, so you can test them with the command: python client.py
 
 # start frontend
 https://scores-shxw.onrender.com/
--- client.py
+++ client.py
@@ -4,23 +4,25 @@ while True:
     print("1) Add customer")
     print("2) Delete customer with id")
     print("3) Fetch all")
-    print("4) Quit")
+    print("4) Fetch scores of your choice")
+    print("5) Fetch score based on ID")
+    print("6) Quit")
 
-    choise = int(input())
+    choice = int(input())
 
     url = 'http://scores-shxw.onrender.com/scores'
 
-    if choise == 1:
+    if choice == 1:
         username = input("Enter the username: ")
         customer_points = int(input("Enter the customers points: "))
         myobj = {'name': username, 'points': customer_points}
         x = requests.post(url, json = myobj)
-        if x.status_code == 201:
+        if x.status_code == 200:
             print("Customer added succesfully")
         else:
             print("Your username is already in use or it is invalid, please choose another.")
         #print(x.status_code)
-    elif choise == 2:
+    elif choice == 2:
         the_id=input("Give the customer id you want to delete:\n")
         d = requests.delete(f"http://scores-shxw.onrender.com/scores/{the_id}?pw=secret")
         if d.status_code == 204:
@@ -28,11 +30,25 @@ while True:
         else:
             print(f"ID {the_id} not found")
         #print(d.status_code)
-    elif choise == 3:
+    elif choice == 3:
         r = requests.get('http://scores-shxw.onrender.com/scores?pw=secret')
         print(r.text)
-    elif choise == 4:
+    elif choice == 4:
+        sort_order = input("Sort in ascending or descending order? (asc/desc): ")
+        limit = int(input("How many top scores do you want to fetch?: "))
+        params = {"sort": sort_order, "limit": limit}
+        r = requests.get(f"{url}?pw=secret", params=params)
+        print(r.text)
+    elif choice == 5:
+        score_id = input("Enter the score id: ")
+        r = requests.get(f"http://scores-shxw.onrender.com/scores/{score_id}?pw=secret")
+        if r.status_code == 200:
+            score = r.json()
+            print(f"Score id: {score['id']}, Name: {score['name']}, Points: {score['points']}")
+        else:
+            print("Score not found.")
+    elif choice == 6:
         print("Bye.")
         break
     else:
-        print("Give a number between 1-4:")
+        print("Give a number between 1-6:")
